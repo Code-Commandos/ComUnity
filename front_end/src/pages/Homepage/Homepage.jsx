ef10b0d1bced267b986fda24fc34c351780742eb
azizurrahman-zero <azizurrahman.zero@gmail.com>
2023-02-17 01:52:10 +0600

    first commit

---
+++ front_end/src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../HomepageCompo/navbar/navbar'
+import "./Homepage.css"
+import Services from '../HomepageCompo/services/services'
+const Homepage = () => {
+  return (
+    <div className='home'>
+      <Navbar />
+      <Services />
+    </div>
+  )
+}
+
+export default Homepage
